1d9c66df3574fd82f77a207aef2c72be172d3122
Maria26Fer <mariafernandadias26@gmail.com>
2024-09-27 12:21:51 -0300

    Alteração

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
 
-
+import IndiretaPai from "./components/comunicacao/IndiretaPai";
+import DiretaPai from "./components/comunicacao/DiretaPai";
 import UsuarioInfo from "./components/condicional/UsuarioInfo";
 import ParOuImpar from "./components/condicional/ParOuImpar";
 import ListaAlunos from "./components/repeticao/ListaAlunos";
@@ -17,52 +18,72 @@ import TabelaProdutos from "./components/repeticao/TabelaProdutos";
 // eslint-disable-next-line
 export default () => (
 
-    <div className="App">
-        <h1>Fundamentos React</h1>
+  <div className="App">
+    <h1>Fundamentos React</h1>
 
-        <div className="Cards">
+    <div className="Cards">
 
-            <Card titulo="#08 - Renderezação Condicional" color="#35c19e">
-                <ParOuImpar numero={20}></ParOuImpar>
-                <UsuarioInfo usuario={{ nome: 'Ferdinando'}}></UsuarioInfo>
-                <UsuarioInfo usuario={{ email: 'ferdinando@gmail.com'}}></UsuarioInfo>
-            </Card>
+    <Card titulo="#10 - Comunicação Indireta" color="#7fc7af">
+        <IndiretaPai></IndiretaPai>
+      </Card>
 
-            <Card titulo="#07 - Desafio Repetição" color="#47c27d">
-                <TabelaProdutos></TabelaProdutos>
-            </Card>
+      <Card titulo="#09 - Comunicação Direta" color="#33a494">
+        <DiretaPai></DiretaPai>
+      </Card>
 
-            <Card titulo="#06 - Repetição" color="#7fc7af">
-                <ListaAlunos></ListaAlunos>
-            </Card>
+      <Card
+        titulo="#08 - Renderezação Condicional"
+        color="#35c19e"
+      >
+        <ParOuImpar
+          numero={20}
+        />
+        <UsuarioInfo
+          usuario={{ nome: 'Ferdinando' }}
+        />
+        <UsuarioInfo
+          usuario={{ email: 'ferdinando@gmail.com' }}
+        />
+      </Card>
 
-            <Card titulo="#5 - Componentes com Filhos" color="#33a494">
-                <Familia sobrenome="Monteiro">
-                    <FamiliaMembro nome="Francisco" />
-                    <FamiliaMembro nome="Joana" />
-                    <FamiliaMembro nome="Lucas" />
-                </Familia>
-            </Card>
+      <Card titulo="#07 - Desafio Repetição" color="#47c27d">
+        <TabelaProdutos></TabelaProdutos>
+      </Card>
 
-            <Card titulo="#4 - Desafio Aleatório" color="#35c19e">
-                <Aleatorio min={1} max={60} />
-            </Card>
+      <Card
+        titulo="#06 - Repetição"
+        color="#7fc7af"
+      >
+        <ListaAlunos />
+      </Card>
 
-            <Card titulo="#3 - Fragmento" color="#47c27d">
-                <Fragmento />
-            </Card>
+      <Card titulo="#5 - Componentes com Filhos" color="#33a494">
+        <Familia sobrenome="Monteiro">
+          <FamiliaMembro nome="Francisco" />
+          <FamiliaMembro nome="Joana" />
+          <FamiliaMembro nome="Lucas" />
+        </Familia>
+      </Card>
 
-            <Card titulo="#2 - Com Parâmetro" color="#7fc7af">
-                <ComParametro
-                    titulo="Situação do Aluno:"
-                    aluno="Creytinho"
-                    nota={10.00}
-                />
-            </Card>
+      <Card titulo="#4 - Desafio Aleatório" color="#35c19e">
+        <Aleatorio min={1} max={60} />
+      </Card>
 
-            <Card titulo="#1 - Primeiro Componente" color="#33a494">
-                <Primeiro></Primeiro>
-            </Card>
-        </div>
+      <Card titulo="#3 - Fragmento" color="#47c27d">
+        <Fragmento />
+      </Card>
+
+      <Card titulo="#2 - Com Parâmetro" color="#7fc7af">
+        <ComParametro
+          titulo="Situação do Aluno:"
+          aluno="Creytinho"
+          nota={10.00}
+        />
+      </Card>
+
+      <Card titulo="#1 - Primeiro Componente" color="#33a494">
+        <Primeiro></Primeiro>
+      </Card>
     </div>
+  </div>
 );
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import "./Card.css"
 // eslint-disable-next-line import/no-anonymous-default-export
-export default props => {
+export default ({color, titulo, children}) => {
 
     const cardStyle = {
-        backgroundColor: props.color || '#F00',
-        borderColor: props.color || '#F00'
+        backgroundColor: color || '#F00',
+        borderColor: color || '#F00'
     }
 
     return (
         <div className="Card" style={cardStyle}>
-            <div className="Title">{props.titulo}</div>
+            <div className="Title">{titulo}</div>
             <div className="Content">
-                {props.children}
+                {children}
             </div>
         </div>
     )
