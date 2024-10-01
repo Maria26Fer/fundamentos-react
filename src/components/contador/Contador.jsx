d446a96b28816a895d88bbaecd34160df157cdb5
Maria26Fer <mariafernandadias26@gmail.com>
2024-10-01 10:46:50 -0300

    Divisão dos componentes do contador;

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,41 +1,44 @@
 import "./Contador.css";
 import React, { Component } from "react";
 
+import Display from "./Display";
+import Botoes from "./Botoes";
+import PassoForm from "./PassoForm";
+
 class Contador extends Component {
- state ={
-  numero: this.props.numeroInicial || 0,
-  passo: this.props.passoInicial || 5,
- };
+    state = {
+        numero: this.props.numeroInicial || 0,
+        passo: this.props.passoInicial || 5,
+    };
 
- incremento = () => {
-  this.setState({ 
-    numero: this.state.numero + this.state.passo,
-  });
- }
+    inc = () => {
+        this.setState({
+            numero: this.state.numero + this.state.passo,
+        });
+    };
 
- decremento = () => {
-  this.setState({ 
-    numero: this.state.numero - this.state.passo,
-  });
- }
+    dec = () => {
+        this.setState({
+            numero: this.state.numero - this.state.passo,
+        });
+    };
 
- setPasso = (evento) => {
-  this.setState({ passo: +evento.target.value, });
- }
-  render() {
-    return(
-      <div className="Contador">
-        <h2>Contador</h2>
-        <h3>{this.state.numero}</h3>
-        <div>
-          <label htmlFor="passoInput">Passo: </label>
-          <input id="passoInput" type="number" 
-          value={this.state.passo} onChange={this.setPasso}></input>
-        </div>
-        <button onClick={this.incremento}> + </button>
-        <button onClick={this.decremento}> - </button>
-      </div>
-    )
-  }
+    setPasso = (novoPasso) => {
+        this.setState({
+            passo: novoPasso,
+        });
+    };
+
+    render() {
+        return (
+            <div className="Contador">
+                <h2>Contador</h2>
+                <Display numero={this.state.numero} />
+                <PassoForm passo={this.state.passo} setPasso={this.setPasso}/>
+                <Botoes setInc={this.inc} setDec={this.dec} />
+            </div>
+        );
+    }
 }
-export default Contador
+
+export default Contador;
